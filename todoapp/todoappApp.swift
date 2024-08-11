ca778d9e77a33cfe85e597a7bf3130c6a7d48b13
tariq2518 <tariq.hussain2518@gmail.com>
2024-08-11 18:35:50 +0500

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Tariq Hussain on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
